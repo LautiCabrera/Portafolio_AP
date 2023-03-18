bb9d162509babe58383c8e45c92060d2c40c1377
Lautaro Cabrera <lau.cabrera114@gmail.com>
2023-03-18 16:37:36 -0300

    Se añade Back-End
    Spring boot, con algunas herramientas.

---
+++ Back-End/src/main/java/com/portfolio/lau/LauApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.lau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LauApplication.class, args);
+	}
+
+}
---
+++ Back-End/src/test/java/com/portfolio/lau/LauApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.lau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
