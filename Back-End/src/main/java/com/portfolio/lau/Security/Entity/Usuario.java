eafb1b1238b8a2684493571b5e68a4ca9f972fcf
Lautaro Cabrera <lau.cabrera114@gmail.com>
2023-03-22 09:06:46 -0300

    Se crea y configura la clase Java "Usuario"
    Dentro del Package Security Entity

--- Back-End/src/main/java/com/portfolio/lau/Security/Entity/Usuario.java
+++ Back-End/src/main/java/com/portfolio/lau/Security/Entity/Usuario.java
@@ -36,6 +36,66 @@ public class Usuario {
     private String contraseña;
     
     @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(name = "usuario_rol", joinColumns = @JoinColumn(name = "usuario_id"), inverseJoinColumns = @JoinColumns(name = "rol_id"))
+    @JoinTable(name = "usuario_rol", joinColumns = @JoinColumn(name = "usuario_id"), inverseJoinColumns = @JoinColumn(name = "rol_id"))
     private Set <Rol> roles = new HashSet<>();
+
+    public Usuario() {
+    }
+
+    public Usuario(String nombre, String nombreUsuario, String email, String contraseña) {
+        this.nombre = nombre;
+        this.nombreUsuario = nombreUsuario;
+        this.email = email;
+        this.contraseña = contraseña;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public Set<Rol> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Rol> roles) {
+        this.roles = roles;
+    }
+    
+    
 }
