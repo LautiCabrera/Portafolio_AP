fb1ad9427d8020672e9e7638d43d20c9fc662780
Lautaro Cabrera <lau.cabrera114@gmail.com>
2023-04-09 15:24:02 -0300

    Se crean componentes.
    Para editar y añadir proyectos.

---
+++ Front-End/src/app/componentes/proyectos/new-habilidad/new-habilidad.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-habilidad',
+  templateUrl: './new-habilidad.component.html',
+  styleUrls: ['./new-habilidad.component.css']
+})
+export class NewHabilidadComponent {
+
+}
