7f91e8907e81dc92c4b975244c839e7abc4f206c
Lautaro Cabrera <lau.cabrera114@gmail.com>
2023-04-16 18:27:56 -0300

    Creación servicio Json

---
+++ Front-End/src/app/service/json.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JsonService {
+
+  constructor(private http : HttpClient) { }
+
+  obtenerDatos(){
+    return this.http.get('assets/json/portfinal.json');
+  }
+  
+}
