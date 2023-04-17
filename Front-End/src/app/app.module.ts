03f1b74e9287a7d759023ec765f5471acb722757
Lautaro Cabrera <lau.cabrera114@gmail.com>
2023-04-17 12:48:44 -0300

    Configuración Firebase.
    Se importan las clases necesarias para implementar el almacenamiento de imágenes en Firebase.

--- Front-End/src/app/app.module.ts
+++ Front-End/src/app/app.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonaService } from './service/persona.service';
+import { environment } from 'src/environments/environments';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule }   from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ContactoComponent } from './componentes/contacto/contacto.component';
 import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
 import { EducacionComponent } from './componentes/educacion/educacion.component';
@@ -9,11 +14,8 @@ import { HabilidadesComponent } from './componentes/habilidades/habilidades.comp
 import { SobreMiComponent } from './componentes/sobre-mi/sobre-mi.component';
 import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
 import { PiePaginaComponent } from './componentes/pie-pagina/pie-pagina.component';
-import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './componentes/home/home.component';
 import { LoginComponent } from './componentes/login/login.component';
-import { FormsModule }   from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
 import { interceptorProvider } from './service/interceptor-service';
 import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
 import { NewExperienciaComponent } from './componentes/experiencia/new-experiencia/new-experiencia.component';
@@ -23,6 +25,19 @@ import { EditEducacionComponent } from './componentes/educacion/edit-educacion/e
 import { NewHabilidadComponent } from './componentes/habilidades/new-habilidad/new-habilidad.component';
 import { EditHabilidadComponent } from './componentes/habilidades/edit-habilidad/edit-habilidad.component';
 import { EditSobreMiComponent } from './componentes/sobre-mi/edit-sobre-mi/edit-sobre-mi.component';
+import { initializeApp } from "firebase/app";
+import { provideFirebaseApp } from '@angular/fire/app';
+import { provideStorage,getStorage } from '@angular/fire/storage';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideFunctions,getFunctions } from '@angular/fire/functions';
+import { provideMessaging,getMessaging } from '@angular/fire/messaging';
+import { providePerformance,getPerformance } from '@angular/fire/performance';
+import { provideRemoteConfig,getRemoteConfig } from '@angular/fire/remote-config';
+import { AngularFireModule } from '@angular/fire/compat';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 @NgModule({
   declarations: [
@@ -50,11 +65,21 @@ import { EditSobreMiComponent } from './componentes/sobre-mi/edit-sobre-mi/edit-
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFireModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideStorage(() => getStorage()),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions()),
+    provideMessaging(() => getMessaging()),
+    providePerformance(() => getPerformance()),
+    provideRemoteConfig(() => getRemoteConfig())
   ],
-  providers: [
-    interceptorProvider
-  ],
+  providers: [PersonaService, interceptorProvider, ScreenTrackingService,UserTrackingService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
