277a2d5969df80a06ab991bb005f81d073f7e49e
Lautaro Cabrera <lau.cabrera114@gmail.com>
2023-04-17 12:50:06 -0300

    Configuración environments
    Se añade el JSON del proyecto Firebase

--- Front-End/src/environments/environments.ts
+++ Front-End/src/environments/environments.ts
@@ -1,5 +1,13 @@
 export const environment = {
 
     production: false,
-    URL: 'http://localhost:8080/'
+    URL: 'http://localhost:8080/',
+    firebase: {
+        apiKey: "AIzaSyBFXO4sWdcV26nQVqT1Rrf_lH1JbbsfyRk",
+        authDomain: "portafolio-ap-fcc99.firebaseapp.com",
+        projectId: "portafolio-ap-fcc99",
+        storageBucket: "portafolio-ap-fcc99.appspot.com",
+        messagingSenderId: "641992546683",
+        appId: "1:641992546683:web:02f11bc1911844232185de"
+      }
 };
